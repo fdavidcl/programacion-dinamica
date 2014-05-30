2d219fe7aedf84fc58d58f6fd4502801b5112a6e
M42 <mromang08@gmail.com>
2014-05-30 14:53:16 +0200

    [Terminales] Implementación completa en Python. Código y pseudocódigo a la vez.

--- src/terminales.py
+++ src/terminales.py
@@ -10,4 +10,13 @@ def nCoins(price):
     if price == 0:
         return 0
     else:
-        return min([1+ nCoins(price - m) for m in coins])
+        return min([1+nCoins(price-m) for m in coins if price-m >= 0])
+
+
+if __name__ == "__main__":
+    print "Introduzca monedas: "
+    coins = map(int, raw_input().split())
+    print "Introduzca precio:  "
+    price = int(input())
+    
+    print "Son necesarias: ", nCoins(price), " monedas"
