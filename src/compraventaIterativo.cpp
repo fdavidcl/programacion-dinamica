1529fbd0560a4a1aa3227bdfa2ec46734f912757
David Charte <fdavidcl@outlook.com>
2014-06-03 19:57:48 +0200

    [compraventa] Ajustes
    Cambiando el vector de sitio para evitar globales y resizes.
    Ajustando algunas declaraciones.

--- src/compraventaIterativo.cpp
+++ src/compraventaIterativo.cpp
@@ -8,81 +8,62 @@ template<class T>
 istream& operator>> (istream& input, vector<T>& v){
     for (auto &i : v)
         input >> i;
-    
+
     return input;
 }
 
-typedef unsigned int uint; 
-
-// Cambio en el struct: Ahora lleva el día de compra y de venta, y calculamos el beneficio a partir de estos datos. 
+// Cambio en el struct: Ahora lleva el día de compra y de venta, y calculamos el beneficio a partir de estos datos.
 typedef struct{
-    uint dia_compra;
-    uint dia_venta;
+    unsigned dia_compra;
+    unsigned dia_venta;
 } Transaccion;
 
-// Cotización de las acciones
-vector<double> precios;
-
-Transaccion resuelve(uint dias){
-    double minimo = precios[0]; 
+Transaccion resuelve(vector<double> precios){
+    unsigned dias = precios.size();
+    double minimo = precios[0];
     double max_beneficio = 0;               // Asumimos que el beneficio inicial es 0
-    double beneficio; 
     int min_index = 0;
-    int indice_compra = 0;  
-    int max_index = 0; 
+    int indice_compra = 0;
+    int max_index = 0;
 
-    for (uint i = 1; i < dias; i++){
+    for (unsigned i = 1; i < dias; i++){
         // Actualizamos el mínimo
-        if (precios[i] < minimo){ 
+        if (precios[i] < minimo){
             minimo = precios[i];
-            min_index = i; 
+            min_index = i;
         }
-        
-        beneficio = precios[i] - minimo; 
 
-        // Si el beneficio es mayor que el mejor beneficio hasta el momento, actualizamos los índices. 
+        double beneficio = precios[i] - minimo;
+
+        // Si el beneficio es mayor que el mejor beneficio hasta el momento, actualizamos los índices.
         if (beneficio > max_beneficio){
-            indice_compra = min_index; 
-            max_beneficio = beneficio; 
-            max_index = i; 
+            indice_compra = min_index;
+            max_beneficio = beneficio;
+            max_index = i;
         }
     }
-    Transaccion venta; 
-    venta.dia_compra = indice_compra; 
-    venta.dia_venta = max_index; 
-    return venta; 
+    Transaccion venta;
+    venta.dia_compra = indice_compra;
+    venta.dia_venta = max_index;
+    return venta;
 }
 
 int main(){
-    
-    uint num_dias;
-    
+    unsigned num_dias;
+
     // Lectura de datos del problema
 
     cout << "Introduce número de días: ";
     cin >> num_dias;
-    precios.resize(num_dias);
+    vector<double> precios(num_dias);
     cout << "Introduce precios de acciones por días: ";
     cin >> precios;
-    
-    Transaccion compraventa = resuelve(num_dias);
-    double beneficio = precios[compraventa.dia_venta] - precios[compraventa.dia_compra]; 
+
+    Transaccion compraventa = resuelve(precios);
+    double beneficio = precios[compraventa.dia_venta] - precios[compraventa.dia_compra];
 
 
     cout << "Día de compra: " << compraventa.dia_compra << endl
          << "Día de venta: " << compraventa.dia_venta << endl
          << "Beneficio: " << beneficio << endl;
 }
-            
-
-
-
-
-
-
-
-
-
-
-
-
