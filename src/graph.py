b28d8a1e7cb5d9098446598a895043b207b4cb8d
M42 <mromang08@gmail.com>
2014-06-04 23:10:06 +0200

    [Grafos] Comentarios a la lectura de grafos.

--- src/graph.py
+++ src/graph.py
@@ -5,6 +5,7 @@
 # Los grafos se representarán como conjunto de vértices y aristas o como
 # matriz de distancias.
 
+
 def readGraph ():
     """ 
     Formato:
@@ -16,6 +17,9 @@ def readGraph ():
     
     Donde "n" es el número de vértices, "e" número de aristas. 
     Y cada arista es del tipo [a,b] con peso w: (a,b):w
+
+    El grafo tendrá nodos [0..n-1], podrá accederse al peso de
+    una arista concreta por: graph[(a,b)]
     """
     
     # Define el grafo
@@ -36,20 +40,26 @@ def readGraph ():
     return graph
 
 
+
 def distanceMatrix(graph):
     """ Da la matriz de distancias de un grafo. """
     def dist(a,b):
+        # Caso de arista contenida en el grafo
         if (a,b) in graph: 
             return graph[(a,b)]
+        # Caso simétrico
         elif (b,a) in graph:
             return graph[(b,a)]
+        # Caso reflexivo
         elif a == b:
             return 0
+        # Caso de ausencia de arista
         else:
             return float('inf')
 
     n = graph['num_vertices']
     return [[dist(a,b) for b in range(n)] for a in range(n)]
+
 
 
 def printMatrix(matrix):
