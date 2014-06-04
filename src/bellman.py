74c77972fb56e0fc45ba3541bc209ef0f990a5d5
David Charte <fdavidcl@outlook.com>
2014-06-04 19:47:51 +0200

    [grafos/bellman-ford] Bellman-Ford en Python

---
+++ src/bellman.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf8
+
+from graph import *
+
+def bellman_ford(graph):
+    # precondición: el grafo no debe contener ciclos de peso negativo
+    n = graph['num_vertices']
+    del graph['num_vertices']
+
+    def algorithm(origen):
+        distancia = [float('inf') for _ in xrange(n)]
+        distancia[origen] = 0
+
+        for _ in xrange(n-1):
+            for (u,v) in graph:
+                if distancia[u] + graph[(u,v)] < distancia[v]:
+                    distancia[v] = distancia[u] + graph[(u,v)]
+
+        return distancia
+
+    mindist = [algorithm(vertice) for vertice in xrange(n)]
+
+    return mindist
+
+
+if __name__ == "__main__":
+    graph = readGraph()
+    matrix = distanceMatrix(graph)
+
+    print "Matriz de distancias:"
+    printMatrix(matrix)
+
+    print "Matriz de longitudes de caminos:"
+    printMatrix(bellman_ford(graph))
