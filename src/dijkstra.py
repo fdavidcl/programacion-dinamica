a88c7e3bcc8a3cec3355f79652a4e1e00c884a5c
David Charte <fdavidcl@outlook.com>
2014-06-05 09:46:18 +0200

    [grafos/dijkstra] Ajuste en comparación

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -7,30 +7,30 @@ def dijkstra_algorithm(graph):
     n = graph['num_vertices']
     del graph['num_vertices']
     w = [[graph[(u,v)] for v in range(n)] for u in range(n)]
-    
+
     def algorithm(s):
         padre = {}
         recorrer = range(n)
         recorrer.remove(s)
-        
+
         d = [ w[s][v] for v in range(n) ]
-        
+
         # Inicializamos los padres
         padre[s] = None
         for v in recorrer:
             if d[v] < float("inf"):
                 padre[v] = s
-        
+
         # Mientras queden nodos por recorrer
         while recorrer:
-            u,v = min( [(d[i], i) for i in recorrer] ) 
-            recorrer.remove(v)            
+            u,v = min( [(d[i], i) for i in recorrer] )
+            recorrer.remove(v)
 
             for a in recorrer:
-                d[a],b = min( [(d[v]+w[v][a],v),(d[a],a)] )
-                if a is not b:
-                    padre[a] = b
-        
+                if d[v] + w[v][a] < d[a]:
+                    d[a] = d[v] + w[v][a]
+                    padre[a] = v
+
         return padre,d
 
     # Devuelve el camino desde el nodo 'origen' hasta i
@@ -40,9 +40,9 @@ def dijkstra_algorithm(graph):
         while (p[i] is not None):
             path += " >- " + str(p[i])
             i = p[i]
-            
+
         return path[::-1]
-        
+
     # Partiendo de todos los nodos aplicamos Dijkstra
     for u in range(n):
         camino,distancia = algorithm(u)
